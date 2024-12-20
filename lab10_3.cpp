06d7b669a3b7c0ab199655ccc76dcb51f7ae5573
THENONT123_ <Nonthaphatc@hotmail.com_>
2024-12-20 14:52:43 +0700

    finish

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    string num;
+    double sum = 0,mean , sd;
+    ifstream in;
+    ofstream out;
+    in.open("score.txt");
+    while(getline(in,num)){
+        sum += atof(num.c_str());
+        n++;
+    }
+    mean = sum/n;
+    in.close();
+
+    sum = 0;
+    in.open("score.txt");
+    while(getline(in,num)){
+        sum += pow(atof(num.c_str()),2);
+    }
+    sd = sqrt((sum/n) - pow(mean,2));
+    in.close();
+
+    cout << "Number of data = "<<n;
+    cout << setprecision(3);
+    cout << "\nMean = "<<mean;
+    cout << "\nStandard deviation = "<<sd;
+}
